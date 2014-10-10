ca16724e329de6581974ad4662a676a0142e1225
Erich Rodrigues <erich.rodrigues@yahoo.com.br>
2014-10-10 13:23:01 -0300

    Adiciona paginação

--- bootstrapform/templatetags/bootstrap.py
+++ bootstrapform/templatetags/bootstrap.py
@@ -1,3 +1,6 @@
+import re
+
+from math import floor
 from django import forms
 from django.template import Context
 from django.template.loader import get_template
@@ -6,6 +9,7 @@ from django import template
 from bootstrapform import config
 
 register = template.Library()
+
 
 @register.filter
 def bootstrap(element):
@@ -24,9 +28,11 @@ def bootstrap_horizontal(element, label_cols={}):
     if not label_cols:
         label_cols = 'col-sm-2 col-lg-2'
 
-    markup_classes = {'label': label_cols,
+    markup_classes = {
+            'label': label_cols,
             'value': '',
-            'single_value': ''}
+            'single_value': ''
+    }
 
     for cl in label_cols.split(' '):
         splited_class = cl.split('-')
@@ -51,8 +57,7 @@ def bootstrap_horizontal(element, label_cols={}):
 
 
 def add_input_classes(field):
-    if not is_checkbox(field) and not is_multiple_checkbox(field) and not is_radio(field) \
-        and not is_file(field):
+    if not is_checkbox(field) and not is_multiple_checkbox(field) and not is_radio(field) and not is_file(field):
         field_classes = field.field.widget.attrs.get('class', '')
         field_classes += ' form-control'
         field.field.widget.attrs['class'] = field_classes
@@ -102,3 +107,103 @@ def is_radio(field):
 @register.filter
 def is_file(field):
     return isinstance(field.field.widget, forms.FileInput)
+
+
+@register.filter
+def pagination(page, pages_to_show=11):
+    """
+    Generate Bootstrap pagination links from a page object
+    """
+    context = get_pagination_context(page, pages_to_show)
+    return get_template("bootstrap_toolkit/pagination.html").render(Context(context))
+
+
+@register.inclusion_tag("bootstrap_toolkit/pagination.html")
+def bootstrap_pagination(page, **kwargs):
+    """
+    Render pagination for a page
+    """
+    pagination_kwargs = kwargs.copy()
+    pagination_kwargs['page'] = page
+    return get_pagination_context(**pagination_kwargs)
+
+
+def get_pagination_context(page, pages_to_show=11, url=None, size=None, align=None, extra=None):
+    """
+    Generate Bootstrap pagination context from a page object
+    """
+    pages_to_show = int(pages_to_show)
+    if pages_to_show < 1:
+        raise ValueError("Pagination pages_to_show should be a positive integer, you specified %s" % pages_to_show)
+    num_pages = page.paginator.num_pages
+    current_page = page.number
+    half_page_num = int(floor(pages_to_show / 2)) - 1
+    if half_page_num < 0:
+        half_page_num = 0
+    first_page = current_page - half_page_num
+    if first_page <= 1:
+        first_page = 1
+    if first_page > 1:
+        pages_back = first_page - half_page_num
+    if pages_back < 1:
+        pages_back = 1
+    else:
+        pages_back = None
+    last_page = first_page + pages_to_show - 1
+    if pages_back is None:
+        last_page += 1
+    if last_page > num_pages:
+        last_page = num_pages
+    if last_page < num_pages:
+        pages_forward = last_page + half_page_num
+        if pages_forward > num_pages:
+            pages_forward = num_pages
+    else:
+        pages_forward = None
+        if first_page > 1:
+            first_page -= 1
+        if pages_back > 1:
+            pages_back -= 1
+        else:
+            pages_back = None
+    pages_shown = []
+    for i in range(first_page, last_page + 1):
+        pages_shown.append(i)
+    # Append proper character to url
+    if url:
+        # Remove existing page GET parameters
+        url = unicode(url)
+        url = re.sub(r'\?page\=[^\&]+', u'?', url)
+        url = re.sub(r'\&page\=[^\&]+', u'', url)
+        # Append proper separator
+        if u'?' in url:
+            url += u'&'
+        else:
+            url += u'?'
+    # Append extra string to url
+    if extra:
+        if not url:
+            url = u'?'
+        url += unicode(extra) + u'&'
+    if url:
+        url = url.replace(u'?&', u'?')
+    # Set CSS classes, see http://twitter.github.io/bootstrap/components.html#pagination
+    pagination_css_classes = ['pagination']
+    if size in ['small', 'large', 'mini']:
+        pagination_css_classes.append('pagination-%s' % size)
+    if align == 'center':
+        pagination_css_classes.append('pagination-centered')
+    elif align == 'right':
+        pagination_css_classes.append('pagination-right')
+    # Build context object
+    return {
+        'bootstrap_pagination_url': url,
+        'num_pages': num_pages,
+        'current_page': current_page,
+        'first_page': first_page,
+        'last_page': last_page,
+        'pages_shown': pages_shown,
+        'pages_back': pages_back,
+        'pages_forward': pages_forward,
+        'pagination_css_classes': ' '.join(pagination_css_classes),
+    }
